b7254ad91580758b2bd2d5d4d441e04f944bee75
kiranoweb <kirano0211>
2020-12-10 15:52:22 +0500

    counters_added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -68,6 +68,33 @@
 		</style>
     <title>Шатура-мебель</title>
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KMNQCSP');</script>
+<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMNQCSP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Marquiz script start -->
+<script src="//script.marquiz.ru/v1.js" type="application/javascript"></script>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  Marquiz.init({ 
+    host: '//quiz.marquiz.ru', 
+    id: '5f672379fd10f300442b6c53', 
+    autoOpen: 12, 
+    autoOpenFreq: 'once', 
+    openOnExit: false 
+  });
+});
+</script>
+<!-- Marquiz script end -->
+
 </head>
 <body>
     <div id="app">
